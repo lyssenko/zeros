d302a455b00c7f743662d0a22da835fa95e2aca4
lyssenko <mutecino@live.com>
2019-10-12 17:27:06 +0600

    Task zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,66 @@
 module.exports = function zeros(expression) {
-  // your solution
-}
+    let a = expression.split('*'),
+        i,
+        summ2 = 0,
+        summ5 = 0,
+        result = 0,
+        factorialis = [],
+        dublFactorialisNeChet = [],
+        dublFactorialisChet = [];
+
+    for (i = 0; i < a.length; i++) {
+        if (a[i].slice(a[i].indexOf('!'), a[i].length) == '!') {
+            factorialis.push(a[i].slice(0, a[i].indexOf('!')));
+        } else if ((a[i].slice(0, a[i].indexOf('!'))) % 2 == true) {
+            dublFactorialisNeChet.push(a[i].slice(0, a[i].indexOf('!')));
+        } else {
+            dublFactorialisChet.push(a[i].slice(0, a[i].indexOf('!')));
+        }
+    }
+
+    for (i = 0; i < factorialis.length; i++) {
+        while (factorialis[i] > 0) {
+            if (factorialis[i] % 2 == 0) {
+                summ2 = summ2 + 1;
+            }
+            if (factorialis[i] % 5 == 0) {
+                summ5 = summ5 + 1;
+            }
+            if (factorialis[i] % 25 == 0) {
+                summ5 = summ5 + 1;
+            }
+            factorialis[i] = factorialis[i] - 1;
+        }
+    }
+
+    for (i = 0; i < dublFactorialisChet.length; i++) {
+        while (dublFactorialisChet[i] > 0) {
+            if (dublFactorialisChet[i] % 2 == 0) {
+                summ2 = summ2 + 1;
+            }
+            if (dublFactorialisChet[i] % 5 == 0) {
+                summ5 = summ5 + 1;
+            }
+            if (dublFactorialisChet[i] % 25 == 0) {
+                summ5 = summ5 + 1;
+            }
+            dublFactorialisChet[i] = dublFactorialisChet[i] - 2;
+        }
+    }
+
+    for (i = 0; i < dublFactorialisNeChet.length; i++) {
+        while (dublFactorialisNeChet[i] > 0) {
+            if (dublFactorialisNeChet[i] % 5 == 0) {
+                summ5 = summ5 + 1;
+            }
+            if (dublFactorialisNeChet[i] % 25 == 0) {
+                summ5 = summ5 + 1;
+            }
+            dublFactorialisNeChet[i] = dublFactorialisNeChet[i] - 2;
+        }
+    }
+
+    result = result + Math.min(summ5, summ2);
+
+    return result;
+};
